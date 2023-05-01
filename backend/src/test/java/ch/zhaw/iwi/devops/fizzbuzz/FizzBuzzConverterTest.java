27c6070d26679ab0436cacd46c661e9123c2ab59
waldesas <waldesas@students.zhaw.ch>
2023-05-01 18:59:03 +0200

    SWA, 01.05.2023: Test addition

--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
@@ -5,29 +5,44 @@ import org.junit.jupiter.api.Test;
 
 public class FizzBuzzConverterTest {
 
-    @Test
-    public void fizzBuzzConverter1() {
-
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-        Assertions.assertEquals("1", fizzBuzz.convert(1));
-
-    }
-    
+  
 
     @Test
-    public void fizzBuzzConvertor2() {
+    public void fizzBuzzConvertor3() {
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-        Assertions.assertEquals("2", fizzBuzz.convert(2));
 
+        Assertions.assertEquals("Fizz", fizzBuzz.convert(3));
+    }
+    
+    @Test
+    public void fizzBuzzConvertorMultiplesOfThree() {
+
+        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+        Assertions.assertEquals("Fizz", fizzBuzz.convert(6));
     }
 
- //    @Test
- //   public void fizzBuzzConvertor3() {
- //
- //      FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
 
- //    Assertions.assertEquals("Fizz", fizzBuzz.convert(3));
- //}
-    
-   
+
+    @Test
+    void Test(){}
+
+    @Test
+    void fizzBuzzConvertorMultiplesOfSeven() {
+
+        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+        Assertions.assertEquals("Buzzy", fizzBuzz.convert(7));
+    }
+
+    @Test
+    void fizzBuzzConvertorMultiplesOfThreeAndSeven() {
+        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+        Assertions.assertNotEquals("FizzBuzz", fizzBuzz.convert(14));
+        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(21));
+        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(42));
+        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(63));
+    }
+
+}
