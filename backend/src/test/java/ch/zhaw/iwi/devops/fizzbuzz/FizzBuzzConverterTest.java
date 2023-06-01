d3e5130a740fcb4b4dad5c55146a37c6bb90006c
waldesas <waldesas@students.zhaw.ch>
2023-06-01 12:01:20 +0200

    SWA, 01.06.2023: Auf ein Test reduziert

--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
@@ -12,46 +12,4 @@ public class FizzBuzzConverterTest {
         Assertions.assertEquals("1", fizzBuzz.convert(1));
 
     }
-
-    @Test
-    public void fizzBuzzConvertor2() {
-
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-        Assertions.assertEquals("2", fizzBuzz.convert(2));
-
-    }
-
-    @Test
-    public void fizzBuzzConvertor3() {
-
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-
-        Assertions.assertEquals("Fizzzzzy", fizzBuzz.convert(3));
-    }
-    
-    @Test
-    public void fizzBuzzConvertorMultiplesOfThree() {
-
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-
-        Assertions.assertEquals("FizzELOU", fizzBuzz.convert(6));
-    }
-
-    @Test
-    void fizzBuzzConvertorMultiplesOfSeven() {
-
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-
-        Assertions.assertEquals("Ich mag Schokolade", fizzBuzz.convert(7));
-    }
-
-    @Test
-    void fizzBuzzConvertorMultiplesOfThreeAndSeven() {
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-        Assertions.assertNotEquals("FizzBuzz", fizzBuzz.convert(14));
-        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(21));
-        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(42));
-        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(63));
-    }
-
 }
